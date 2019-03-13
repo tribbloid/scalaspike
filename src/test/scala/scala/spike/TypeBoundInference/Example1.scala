9f6152d4a80d95f0dacfa55f02a901e6cb633928
peng <pc175@uow.edu.au>
2019-03-13 16:19:10 -0400

    fix example1 in TypeBoundInference

--- src/test/scala/scala/spike/TypeBoundInference/Example1.scala
+++ src/test/scala/scala/spike/TypeBoundInference/Example1.scala
@@ -1,23 +1,41 @@
-//package scala.spike.TypeBoundInference
-//
-//object Example1 {
-//
-//  trait Domain {}
-//
-//  trait Impl {
-//
-//    type DD <: Domain
-//    type GG <: StaticGraph[DD]
-//
-//    final type Builder = Example1.Builder[DD, GG]
-//  }
-//
-//  trait DSL[I <: Impl] {
-//
+package scala.spike.TypeBoundInference
+
+import scala.language.existentials
+
+object Example1 {
+
+  trait Domain {}
+  trait D1 extends Domain
+
+  trait Impl {
+
+    type DD <: Domain
+    type GG <: StaticGraph[DD]
+
+    final type Builder = Example1.Builder[DD, GG]
+  }
+
+  trait DSL[I <: Impl] {
+
+    //this will fail
 //    val impl: Builder[I#DD, I#GG]
-//  }
-//
-//  trait StaticGraph[T <: Domain] {}
-//
-//  trait Builder[D <: Domain, G <: StaticGraph[D]] {}
-//}
+
+    val impl: Builder[i.DD, i.GG] forSome { val i: Impl }
+  }
+
+  trait StaticGraph[T <: Domain] {}
+
+  trait Builder[D <: Domain, G <: StaticGraph[D]] {}
+
+  val i1: Impl = new Impl {
+
+    type DD = Domain
+    type GG = StaticGraph[Domain]
+  }
+
+  val i2: Impl = new Impl {
+
+    type DD = D1
+    type GG = StaticGraph[D1]
+  }
+}
