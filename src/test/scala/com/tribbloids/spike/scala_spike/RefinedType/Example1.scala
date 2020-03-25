999af739681c6838eca6e12f674440e1aedeebf0
peng <pc175@uow.edu.au>
2020-03-25 17:37:44 -0400

    add the old Example for shapeless refined type

--- src/test/scala/com/tribbloids/spike/scala_spike/RefinedType/Example1.scala
+++ src/test/scala/com/tribbloids/spike/scala_spike/RefinedType/Example1.scala
@@ -1,1 +1,76 @@
+package com.tribbloids.spike.scala_spike.RefinedType
 
+import org.scalatest.FunSpec
+import shapeless.Witness
+import shapeless.Witness._
+
+import com.tribbloids.spike.scala_spike.RefinedType.Example1._
+
+//TODO: already have shapeless spike for this
+object Example1 {
+
+  //  val cache
+  //
+  //  def wit(v: Int) =
+
+  val two: Lt[Int] = Witness(2)
+  val three: Lt[Int] = Witness(3)
+
+  val alsoTwo: Lt[Int] = Witness(2)
+
+  assert(two.getClass.isAssignableFrom(alsoTwo.getClass))
+
+  // SU means 'special unitary group'
+
+  class ComplexVector[D <: Lt[Int]](
+      val vs: List[(Double, Double)]
+  )(
+      implicit ev: Aux[D]
+  ) {
+
+    {
+      val d = vs.size
+      assert(ev.value.value == d)
+    }
+
+    def +(v2: ComplexVector[D])(
+        implicit ev2: D <:< two.type
+    ): ComplexVector[D] = {
+      val newVs = this.vs.zip(v2.vs).map {
+        case (t1, t2) =>
+          (t1._1 + t2._1) -> (t1._2 + t2._2)
+      }
+      new ComplexVector[D](newVs)
+    }
+  }
+
+  type Quaternion = ComplexVector[two.type]
+
+}
+
+class Example1 extends FunSpec {
+
+  it("2d") {
+
+    val q = new Quaternion(List(1.0 -> 2.0, 3.0 -> 4.0))
+    val q2 = new ComplexVector[alsoTwo.type](List(3.0 -> 4.0, 1.0 -> 2.0))
+
+    q + q
+
+    //TODO: how to make it work?
+    //    q + q2
+    //
+    //    q2 + q2
+  }
+
+  it("3d") {
+
+    val vec =
+      new ComplexVector[three.type](List(1.0 -> 2.0, 3.0 -> 4.0, 5.0 -> 6.0))
+
+    // This will break
+    //  {
+    //    val v3 = SU_3.plus(vec, vec)
+    //  }
+  }
+}
