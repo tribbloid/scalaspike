a938668ba69841d10f52fe8a7a9fa0918a90674d
peng <pc175@uow.edu.au>
2020-03-25 17:35:54 -0400

    merging 2 diverging packages

--- src/test/scala/com/tribbloids/spike/scala_spike/Overriding/InnerClassOverridingAbstractType.scala
+++ src/test/scala/com/tribbloids/spike/scala_spike/Overriding/InnerClassOverridingAbstractType.scala
@@ -1,1 +1,21 @@
+package com.tribbloids.spike.scala_spike.Overriding
 
+object InnerClassOverridingAbstractType {
+
+  trait AALike
+
+  trait SS {
+
+    type AA <: AALike
+  }
+
+  class SSA extends SS {
+
+    case class AA() extends AALike
+  }
+
+  object SSB extends SS {
+
+    case class AA() extends AALike
+  }
+}
--- src/test/scala/com/tribbloids/spike/scala_spike/Overriding/InnerObjectOverridingMethodOrProperty.scala
+++ src/test/scala/com/tribbloids/spike/scala_spike/Overriding/InnerObjectOverridingMethodOrProperty.scala
@@ -1,1 +1,48 @@
+package com.tribbloids.spike.scala_spike.Overriding
 
+object InnerObjectOverridingMethodOrProperty {
+
+  trait AALike
+
+  object AA_Outer extends AALike
+
+  trait SS1 {
+
+    def AA: AALike
+  }
+
+  trait SS_Mixin {
+
+    object AA extends AALike
+  }
+
+  case class SSA() extends SS1 with SS_Mixin
+
+  // cannot override final member
+  //  object SSA extends SSA {
+  //
+  //    object AA extends AALike
+  //  }
+
+  trait SS2 {
+
+    val AA: AALike
+  }
+
+  case class SSB() extends SS2 with SS_Mixin
+
+  // cannot override final member
+  //  object SSB extends SSB {
+  //
+  //    object AA extends AALike
+  //  }
+
+  trait SS3 {
+    val AA: AALike = AA_Outer
+  }
+  case class SSC() extends SS3 {
+
+    override object AA extends AALike
+  }
+
+}
