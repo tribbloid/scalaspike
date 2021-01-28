d1856e7ac3fb118b32b4b0f97f76a47542a3c24e
Peng Cheng <pc175@uowmail.edu.au>
2021-01-27 19:27:04 -0500

    add several examples

--- common/src/test/scala/com/tribbloids/spike/shapeless_spike/RecordSuite.scala
+++ common/src/test/scala/com/tribbloids/spike/shapeless_spike/RecordSuite.scala
@@ -31,13 +31,53 @@ class RecordSuite extends BaseSpec {
     val _vv = _v: Values.Aux[bookW.Wide, _v.Out]
     val v2 = book.values(_vv)
 
-//    WideTyped(v2).viz.toString.shouldBe() // TODO: this is wrong
-
-//    assert(v2.head == "Benjamin Pierce")
   }
 
   it("lookup") {
 
     print_@(WideTyped(book).viz)
   }
+
+  describe("error cases") {
+
+    val record = {
+      ("a" ->> 1) ::
+        ("b" ->> 2) ::
+        HNil
+    }
+
+    def inferKeys[T <: HList](v: T)(implicit keys: shapeless.ops.record.Keys[T]) = keys
+
+    it("1") {
+
+      {
+        val keys = record.keys // works
+        print(keys)
+
+        inferKeys(record) // works
+      }
+
+      {
+        val record2: record.type = record
+        val keys = record2.keys // works
+        print(keys)
+
+        inferKeys(record2) // works ?
+//        inferKeys[record.type](record2) // compilation error!
+      }
+    }
+
+    it("2") {
+
+      {
+        type RR = record.type
+
+        val record2: RR = record
+
+//        inferKeys(record2)
+//        inferKeys[record.type](record2)
+      }
+    }
+  }
+
 }
