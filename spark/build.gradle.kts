e9bdba797d7d28b0d2c1ada7002f8f1451729bbf
Peng Cheng <peng.cheng@acyclic.ai>
2025-12-20 20:21:35 -0500

    upgrade dependencies

--- spark/build.gradle.kts
+++ spark/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     testImplementation("org.apache.spark:spark-yarn_${vs.scala.binaryV}:${vs.spark.v}")
 
 //    implementation("org.apache.spark:spark-sql-kafka-0-10_${vs.scala.binaryV}:${vs.spark.v}")
-    implementation("io.github.embeddedkafka:embedded-kafka-streams_${vs.scala.binaryV}:3.9.0")
+    implementation("io.github.embeddedkafka:embedded-kafka-streams_${vs.scala.binaryV}:4.1.0")
 
 //    implementation("uk.co.gresearch.spark:spark-extension_2.13:2.12.0-3.5")
 }
