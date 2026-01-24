3fd3857e4322987a5624beb559ceb0c3734a6a33
pengcheng <peng.cheng@acyclic.ai>
2026-01-24 13:09:17 -0500

    add DeriveZlayerSpec

--- cats3/build.gradle.kts
+++ cats3/build.gradle.kts
@@ -10,7 +10,9 @@ dependencies {
     api(project(":common"))
     testFixturesApi(testFixtures(project(":common")))
 
-    implementation("org.typelevel:cats-effect_${vs.scala.binaryV}:3.5.0-RC3")
-    implementation("org.typelevel:cats-effect-std_${vs.scala.binaryV}:3.5.0-RC3")
+    val catsEffectV = "3.5.0-RC3"
+
+    implementation("org.typelevel:cats-effect_${vs.scala.binaryV}:$catsEffectV")
+    implementation("org.typelevel:cats-effect-std_${vs.scala.binaryV}:$3.5.0-RC3")
 //    implementation("org.typelevel:cats-effect-cps_${vs.scala.binaryV}:0.4.0")
 }
---
+++ zio/src/test/scala/com/tribbloids/spike/zio_spike/DeriveZlayerSpec.scala
@@ -0,0 +1,43 @@
+package com.tribbloids.spike.zio_spike
+
+import ai.acyclic.prover.commons.refl.Reflection.Runtime.TypeTag
+import ai.acyclic.prover.commons.testlib.BaseSpec
+import zio.*
+
+class DeriveZlayerSpec extends BaseSpec {
+
+  import DeriveZlayerSpec.*
+
+  def printOrigin[I, O](layer: ZLayer[I, Any, O])(
+    implicit
+    iTag: TypeTag[I],
+    oTag: Tag[O]
+  ): String = {
+    val result = iTag.tpe.toString + " -> " + oTag.tag.toString()
+
+    result
+  }
+
+  it("derive") {
+
+    val layer = ZLayer.derive[Goto]
+
+    val origin = printOrigin(layer)
+    assert(origin.contains(Goto.getClass.getName.stripSuffix("$")))
+  }
+
+  it("fromFunction") {
+
+    val layer = ZLayer.fromFunction((s: String, e: Env) => Goto(s)(e))
+
+    val origin = printOrigin(layer)
+    assert(origin.contains(Goto.getClass.getSimpleName.stripSuffix("$")))
+  }
+}
+
+object DeriveZlayerSpec {
+
+  trait Env
+  case class Goto(address: String)(val env: Env) {}
+
+}
